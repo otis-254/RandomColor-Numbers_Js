51b1bdeeb90ca27d96d4bcc96b37d3512cb01e97
Caleb Olele <olelecaleb176@gmail.com>
2023-10-30 14:07:00 +0300

    R.color and number

---
+++ script.js
@@ -0,0 +1,28 @@
+const number = document.querySelector(".number");
+const btn = document.querySelector(".generate");
+
+
+const generateNumber = () => {
+    const randomNumber = Math.floor(Math.random() * 50 + 1);
+    number.innerHTML = randomNumber;
+};
+
+
+btn.addEventListener("click", generateNumber);
+
+generateNumber();
+
+
+
+// const rand = Math.floor(Math.random() * 10);
+// console.log("rand");
+
+
+
+
+
+
+
+
+
+
